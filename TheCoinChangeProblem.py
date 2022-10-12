ae17b81434a58a31fe659ae85ea535f8d0ca20ab
Chrisler Nunes <69667515+chrislernunes@users.noreply.github.com>
2022-10-12 23:31:59 +0530

    Create TheCoinChangeProblem.py

---
+++ TheCoinChangeProblem.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+coins = list(map(int, input().split()))
+memo = [0] * (n + 1)
+memo[0] = 1
+
+for coin in coins:
+    for change_ind in range(coin, n + 1):
+        memo[change_ind] += memo[change_ind - coin]
+
+print(memo[n])
